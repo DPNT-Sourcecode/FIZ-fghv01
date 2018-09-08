4e133815ad03e2e683b2647a947250e095453ada
sbx_user1059 <sbx_user1059@ip-10-129-121-55.eu-west-2.compute.internal>
2018-09-08 13:33:40 +0000

    Sat Sep 08 13:33:40 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,11 +1,29 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        String answer = null;
+        if (isFizz(number)) {
+            answer = "Fizz ";
+        }
+        if (isBuzz(number)) {
+            answer += "Buzz";
+        }
+        if (null != answer) {
+            return answer.trim();
+        }
+        return number.toString();
+    }
+
+    // No remainder after division by three
+    private boolean isFizz(Integer number) {
+        return number % 3 == 0;
+    }
+
+    // No remainder after division by five
+    private boolean isBuzz(Integer number) {
+        return number % 5 == 0;
     }
 
 }
